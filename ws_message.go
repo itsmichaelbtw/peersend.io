d26706cd4c958bb8b4a40e29b7070359bb5cfa8c
itsmichaelbtw <itsmichaelbtw@gmail.com>
2025-03-09 18:01:35 +1100

    feat: add ws message utility function

---
+++ ws_message.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type MessageType string
+
+const (
+	SessionMessage MessageType = "session"
+	SignalMessage  MessageType = "signal"
+	ErrorMessage   MessageType = "error"
+)
+
+type Message struct {
+	Type MessageType    `json:"type"`
+	Data map[string]any `json:"data"`
+}
+
+func ParseMessage(data []byte) (*Message, error) {
+	var msg Message
+	if err := json.Unmarshal(data, &msg); err != nil {
+		return nil, fmt.Errorf("invalid message format: %v", err)
+	}
+	return &msg, nil
+}
+
+func CreateMessage(msgType MessageType, data map[string]any) []byte {
+	msg := Message{
+		Type: msgType,
+		Data: data,
+	}
+
+	bytes, err := json.Marshal(msg)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to marshal message: %v", err))
+	}
+
+	return bytes
+}
