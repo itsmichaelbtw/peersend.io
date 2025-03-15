e07f4b5ef69fbcdd73aab0a2289ee8f2cebb0840
itsmichaelbtw <itsmichaelbtw@gmail.com>
2025-03-15 16:41:19 +1100

    feat: add main and session layout components

---
+++ web/src/layouts/MainLayout.vue
@@ -0,0 +1,10 @@
+<script>
+import MainHeader from "@/components/header/MainHeader.vue";
+</script>
+
+<template>
+  <MainHeader />
+  <main class="content">
+    <router-view />
+  </main>
+</template>
---
+++ web/src/layouts/SessionLayout.vue
@@ -0,0 +1,8 @@
+<script>
+import SessionHeader from "@/components/header/SessionHeader.vue";
+</script>
+
+<template>
+  <SessionHeader />
+  <router-view />
+</template>
