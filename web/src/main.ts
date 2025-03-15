867f26ad790380278e134bf5a8b3db36ba1edbd9
itsmichaelbtw <itsmichaelbtw@gmail.com>
2025-03-15 14:10:01 +1100

    feat: set primary theme for primevue

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,11 +5,29 @@ import { router } from "./router";
 
 import Aura from "@primeuix/themes/aura";
 import PrimeVue from "primevue/config";
-import Button from "primevue/button";
 
 import StyleClass from "primevue/styleclass";
 
 import PeerSend from "./PeerSend.vue";
+import { definePreset } from "@primeuix/themes";
+
+const theme = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: "{blue.50}",
+      100: "{blue.100}",
+      200: "{blue.200}",
+      300: "{blue.300}",
+      400: "{blue.400}",
+      500: "{blue.500}",
+      600: "{blue.600}",
+      700: "{blue.700}",
+      800: "{blue.800}",
+      900: "{blue.900}",
+      950: "{blue.950}"
+    }
+  }
+});
 
 const app = createApp(PeerSend);
 
@@ -17,12 +35,11 @@ app
   .use(router)
   .use(PrimeVue, {
     theme: {
-      preset: Aura,
+      preset: theme,
       options: {
         darkModeSelector: ".p-dark"
       }
     }
   })
-  .component("Button", Button)
   .directive("styleclass", StyleClass)
   .mount("#app");
