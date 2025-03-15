0a7c90f24d16814606b4af485c4d5bdabcdeacb3
itsmichaelbtw <itsmichaelbtw@gmail.com>
2025-03-15 16:21:21 +1100

    feat: add "geist" custom font

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,4 +1,5 @@
 import "./assets/css/tailwindcss.css";
+import "./assets/css/fonts.css";
 
 import { createApp } from "vue";
 import { router } from "./router";
@@ -9,25 +10,6 @@ import PrimeVue from "primevue/config";
 import StyleClass from "primevue/styleclass";
 
 import PeerSend from "./PeerSend.vue";
-import { definePreset } from "@primeuix/themes";
-
-const theme = definePreset(Aura, {
-  semantic: {
-    primary: {
-      50: "{blue.50}",
-      100: "{blue.100}",
-      200: "{blue.200}",
-      300: "{blue.300}",
-      400: "{blue.400}",
-      500: "{blue.500}",
-      600: "{blue.600}",
-      700: "{blue.700}",
-      800: "{blue.800}",
-      900: "{blue.900}",
-      950: "{blue.950}"
-    }
-  }
-});
 
 const app = createApp(PeerSend);
 
@@ -35,7 +17,7 @@ app
   .use(router)
   .use(PrimeVue, {
     theme: {
-      preset: theme,
+      preset: Aura,
       options: {
         darkModeSelector: ".p-dark"
       }
