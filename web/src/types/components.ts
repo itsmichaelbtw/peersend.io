59bf8ce9d8aec7c2ab1f21db8c3882efcc4075c9
itsmichaelbtw <itsmichaelbtw@gmail.com>
2025-03-15 17:51:56 +1100

    components.ts: add `ComponentTypes` namespace with `SessionProps.Container` interface

---
+++ web/src/types/components.ts
@@ -0,0 +1,12 @@
+export namespace ComponentTypes {
+  export namespace SessionProps {
+    export interface Container {
+      heading: string;
+      alternative: {
+        text: string;
+        link: string;
+        linkText: string;
+      };
+    }
+  }
+}
