d78a29bd80cd2bc76427021250f1b1d0199d5f62
itsmichaelbtw <itsmichaelbtw@gmail.com>
2025-03-18 20:34:25 +1100

    feat: add toast notifications for websocket errors in session layout

--- web/src/layouts/SessionLayout.vue
+++ web/src/layouts/SessionLayout.vue
@@ -1,11 +1,32 @@
-<script>
+<script setup>
 import SessionHeader from "@/components/header/SessionHeader.vue";
+
+import { watch } from "vue";
+import { websocketState } from "@/state/websocket";
+
+import { useToast } from "primevue/usetoast";
+
+const toast = useToast();
+
+watch(
+  () => websocketState.last_error,
+  (newError) => {
+    if (newError) {
+      toast.add({
+        severity: "error",
+        detail: newError.message,
+        life: 2500,
+        summary: newError.type
+      });
+    }
+  }
+);
 </script>
 
 <template>
   <SessionHeader />
   <main
-    class="container mx-auto flex flex-col items-center justify-center gap-4 py-12 not-sm:py-6 md:py-24 lg:py-32"
+    class="mx-auto flex max-w-6xl flex-col items-center justify-center gap-4 py-12 not-sm:py-6 md:py-24 lg:py-32"
   >
     <router-view />
   </main>
