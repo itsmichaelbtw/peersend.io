0abf993835393cd841ea7275ce04e31a4009c97e
itsmichaelbtw <itsmichaelbtw@gmail.com>
2025-04-05 13:10:35 +1100

    ws_http.go: add http query extraction functionality

---
+++ ws_http.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+type HttpQuery struct {
+	sessionCode string
+	mode        string
+}
+
+func ExtractHttpQuery(r *http.Request) *HttpQuery {
+	query := r.URL.Query()
+
+	return &HttpQuery{
+		sessionCode: query.Get("sessionCode"),
+		mode:        query.Get("mode"),
+	}
+}
