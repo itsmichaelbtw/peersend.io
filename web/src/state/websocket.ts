b936616d0399b94c6107fca2af70561d16dc7753
itsmichaelbtw <itsmichaelbtw@gmail.com>
2025-04-09 20:17:32 +1000

    websocket.ts: add `clients` and `maximum_clients` properties, update `last_error` handling

--- web/src/state/websocket.ts
+++ web/src/state/websocket.ts
@@ -5,8 +5,11 @@ import { reactive } from "vue";
 export const websocketState = reactive<WebSocketState>({
   ws: null,
   session_code: null,
+  clients: [],
   is_host: false,
   is_connected: false,
+  is_connecting: false,
   client_id: null,
-  last_error_message: null
+  maximum_clients: 0,
+  last_error: null
 });
