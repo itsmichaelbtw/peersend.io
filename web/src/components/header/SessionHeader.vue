290e8ec1fda40a0c55c73c5e5666c2d5486089fc
itsmichaelbtw <itsmichaelbtw@gmail.com>
2025-04-19 16:52:27 +1000

    feat: add host and connection badges to session header

--- web/src/components/header/SessionHeader.vue
+++ web/src/components/header/SessionHeader.vue
@@ -1,5 +1,33 @@
+<script setup lang="ts">
+import AppLogo from "../AppLogo.vue";
+
+import { UsersRound, Crown } from "lucide-vue-next";
+import { websocketState } from "@/state/websocket";
+</script>
+
 <template>
   <header class="border-b border-gray-200">
-    <div class="container flex h-16 items-center justify-between px-4 md:px-6"></div>
+    <div class="container mx-auto flex h-16 items-center justify-between px-4 md:px-6">
+      <AppLogo />
+
+      <template v-if="websocketState.session_code && websocketState.is_connected">
+        <div class="select-none">
+          <div class="flex items-center justify-center gap-x-2">
+            <Badge v-if="websocketState.is_host" size="large" severity="secondary">
+              <Crown class="mr-2 inline align-middle" v-bind:size="16" />
+              <span class="inline-block align-middle">Host</span>
+            </Badge>
+
+            <Badge size="large" severity="secondary">
+              <UsersRound class="mr-2 inline align-middle" v-bind:size="16" />
+              <span class="inline-block align-middle">
+                {{ websocketState.clients.length }}/{{ websocketState.maximum_clients }}
+                Connected
+              </span>
+            </Badge>
+          </div>
+        </div>
+      </template>
+    </div>
   </header>
 </template>
