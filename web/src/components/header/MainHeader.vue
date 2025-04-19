a00961790ef712e79678553e88f1aedbb90adeb5
itsmichaelbtw <itsmichaelbtw@gmail.com>
2025-04-19 16:51:51 +1000

    fix: middle align header horizontally

--- web/src/components/header/MainHeader.vue
+++ web/src/components/header/MainHeader.vue
@@ -4,7 +4,7 @@ import AppLogo from "@/components/AppLogo.vue";
 
 <template>
   <header class="border-b border-gray-200">
-    <div class="container flex h-16 items-center justify-between px-4 md:px-6">
+    <div class="container mx-auto flex h-16 items-center justify-between px-4 md:px-6">
       <AppLogo />
     </div>
   </header>
