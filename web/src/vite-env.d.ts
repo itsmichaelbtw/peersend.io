d98e4ca40fd188cc5fd3ac4941c8e25fc60fde2b
itsmichaelbtw <itsmichaelbtw@gmail.com>
2025-04-26 15:29:55 +1000

    chore: misc updates to web build

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ViteTypeOptions {}
+
+interface ImportMetaEnv {
+  readonly VITE_SESSION_CODE_EXAMPLE: string;
+  readonly VITE_SERVER_ENDPOINT: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
