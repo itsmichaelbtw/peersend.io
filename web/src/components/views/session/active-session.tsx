2c378296d825110992248c9c14825a41b74210a0
itsmichaelbtw <itsmichaelbtw@gmail.com>
2025-09-14 12:22:17 +1000

    feat: implement main layout, session layout, compatibility view, active session view, create session view, and join session view with routing

---
+++ web/src/components/views/session/active-session.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+import { TriangleAlertIcon } from "lucide-react";
+import { Alert } from "@mantine/core";
+import { useAppState } from "@/hooks/use-app-state";
+
+import { SessionStatusBar } from "@/components/session-status-bar";
+import { ConnectionStatus } from "@/components/containers/connection-status";
+import { Collaboration } from "@/components/containers/collaboration";
+
+export function ActiveSessionView() {
+  const { sessionState } = useAppState();
+
+  const [showAlert, setShowAlert] = useState(true);
+
+  if (!sessionState.isConnected || sessionState.connectionType === "none") {
+    return <p>You are not connected</p>;
+  }
+
+  return (
+    <React.Fragment>
+      {showAlert && (
+        <Alert
+          icon={<TriangleAlertIcon />}
+          color="yellow"
+          className="w-full select-none"
+          closeButtonLabel="Dismiss"
+          autoContrast
+          withCloseButton
+          onClose={() => {
+            setShowAlert(false);
+          }}
+        >
+          <span className="text-[var(--mantine-color-yellow-text)]">
+            All received files will be lost when you close this tab or browser. Download any files
+            you want to keep.
+          </span>
+        </Alert>
+      )}
+
+      <SessionStatusBar />
+
+      <div className="relative w-full grid grid-cols-3 gap-4">
+        <div className="col-span-1 space-y-4">
+          <ConnectionStatus />
+        </div>
+
+        <div className="col-span-2">
+          <Collaboration />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
---
+++ web/src/router/index.tsx
@@ -0,0 +1,80 @@
+import { MainLayout } from "@/components/layouts/main-layout";
+import { SessionLayout } from "@/components/layouts/session-layout";
+
+import { CompatibilityView } from "@/components/views/compatiblity";
+import { CreateSessionView } from "@/components/views/session/create-session";
+import { JoinSessionView } from "@/components/views/session/join-session";
+import { ActiveSessionView } from "@/components/views/session/active-session";
+
+import { isBrowserCompatible } from "@/hooks/use-compatibility";
+import { createBrowserRouter, redirect } from "react-router";
+import { getAppState } from "@/state/app-state";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: () => <div>Home Page</div>
+      }
+    ]
+  },
+  {
+    path: "/compatibility",
+    Component: CompatibilityView
+  },
+  {
+    path: "/session",
+    Component: SessionLayout,
+    loader() {
+      if (isBrowserCompatible()) {
+        return null;
+      }
+
+      throw redirect("/compatibility");
+    },
+    children: [
+      {
+        index: true,
+        loader() {
+          throw redirect("/session/create");
+        }
+      },
+      {
+        path: "create",
+        loader() {
+          const { sessionState } = getAppState();
+
+          if (sessionState.isConnected && sessionState.sessionCode) {
+            throw redirect(`/session/${sessionState.sessionCode}`);
+          }
+        },
+        element: <CreateSessionView />
+      },
+      {
+        path: "join",
+        loader() {
+          const { sessionState } = getAppState();
+
+          if (sessionState.isConnected && sessionState.sessionCode) {
+            throw redirect(`/session/${sessionState.sessionCode}`);
+          }
+        },
+        element: <JoinSessionView />
+      },
+      {
+        path: ":session_code",
+        loader() {
+          const { sessionState } = getAppState();
+
+          if (!sessionState.isConnected || sessionState.connectionType === "none") {
+            throw redirect("/session/create");
+          }
+        },
+        element: <ActiveSessionView />
+      }
+    ]
+  }
+]);
