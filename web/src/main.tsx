67e3f53ef0243bf5b21a3ea14a7e245ad96f94da
itsmichaelbtw <itsmichaelbtw@gmail.com>
2025-09-16 17:52:02 +1000

    feat: add mantine providers

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,46 @@
-import "./css/tailwind.css";
+import "./assets/css/tailwind.css";
+import "./assets/css/fonts.css";
+
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
+import "@mantine/dropzone/styles.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import { router } from "./router";
 
-import App from "./App.tsx";
+import { MantineProvider } from "@mantine/core";
+import { ModalsProvider } from "@mantine/modals";
+import { Notifications } from "@mantine/notifications";
+import { ConnectionUpgrade } from "./components/containers/collaboration";
 
-createRoot(document.getElementById("root")!).render(
+const root = document.getElementById("root")!;
+
+const modals = {
+  initiateDirectConnection: ConnectionUpgrade
+};
+
+declare module "@mantine/modals" {
+  export interface MantineModalsOverride {
+    modals: typeof modals;
+  }
+}
+
+createRoot(root).render(
   <StrictMode>
-    <App />
+    <MantineProvider
+      withCssVariables
+      forceColorScheme="light"
+      theme={{
+        primaryColor: "teal",
+        fontFamily: "Geist"
+      }}
+    >
+      <ModalsProvider modals={modals}>
+        <Notifications />
+        <RouterProvider router={router} />
+      </ModalsProvider>
+    </MantineProvider>
   </StrictMode>
 );
