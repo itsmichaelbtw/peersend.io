18a5ac77fa8eade9bceda381309d4b9f38303d03
itsmichaelbtw <itsmichaelbtw@gmail.com>
2025-10-27 16:09:36 +1100

    typo on event error

--- server/internal/events/errors.go
+++ server/internal/events/errors.go
@@ -2,4 +2,4 @@ package events
 
 import "errors"
 
-var ErrNoHandlerRegistered = errors.New("no handler reigstered for message type")
+var ErrNoHandlerRegistered = errors.New("no handler registered for message type")
